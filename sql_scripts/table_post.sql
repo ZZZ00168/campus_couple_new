f2c38ad6dad5eb108f488a409505d4f72389af87
Alex Valentine <farseer810@163.com>
2015-08-12 19:22:23 +0800

    finished campus_list.py

--- sql_scripts/table_post.sql
+++ sql_scripts/table_post.sql
@@ -1,21 +1,17 @@
 use campus_couple;
 drop table if exists post;
 create table post(
-	post_id tinyint primary key auto_increment,
-	add_time TIMESTAMP DEFAULT now() ,
-	user_id tinyint not null,
-	content varchar(140)
-)engine=InnoDB default charset=utf8;
-
-drop table if exists post_img;
-create table post_img(
-	post_id int ,
-	img_url varchar(100)
+	post_id int primary key auto_increment,
+	add_time TIMESTAMP DEFAULT now(),
+	user_id int not null,
+	content varchar(200) not null,
+	img_url varchar(100),
+	thumbnail_img_url varchar(100)
 )engine=InnoDB default charset=utf8;
 
 drop table if exists favor;
 create table favor(
-	post_id int ,
+	post_id int,
 	user_id int
 )engine=InnoDB default charset=utf8;
 
@@ -25,7 +21,7 @@ create table comments(
 	user_id int not NULL ,#评论者的id
 	comment_id int PRIMARY KEY AUTO_INCREMENT,
 	commented_id int, #被评论的评论者id
-	content varchar(60) ,
+	content varchar(60),
 	add_time TIMESTAMP
 )engine=InnoDB default charset=utf8;
 
