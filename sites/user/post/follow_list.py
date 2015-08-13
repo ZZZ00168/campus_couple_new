181a99102a371d7300b3a1d63962740667b4f228
Alex Valentine <farseer810@163.com>
2015-08-13 21:43:05 +0800

    fix follow/list and follow/update

--- sites/user/post/follow_list.py
+++ sites/user/post/follow_list.py
@@ -20,114 +20,130 @@ from verify import *
 @route.route('/user/post/follow/list')
 class UserPostFollowList:  # 获取关注用户的文章列表
     def POST(self):  # 传入access_token,user_id,start_poost_id,start_index,end_index
-        input = web.input(access_token=None, user_id=None, start_post_id=None, start_index=None, end_index=None, )
+        input = web.input(access_token=None, user_id=None, start_post_id=None,
+                          start_index=None, post_count=None)
 
         # 缺少必填参数
-        if input.access_token == None or input.user_id == None or input.start_post_id == None or input.start_index == None or input.end_index == None:
-            return output(110, False)
+        if(input.access_token == None or input.user_id == None or input.start_post_id == None or
+                   input.start_index == None or input.post_count == None):
+            return output(110)
         try:
             input.user_id = int(input.user_id)
             input.start_post_id = int(input.start_post_id)
             input.start_index = int(input.start_index)
-            input.end_index = int(input.end_index)
-            if input.start_index < 0 or input.start_index > input.end_index:  # 参数值非法
-                return output(112, False)
+            input.post_count = int(input.post_count)
+            if input.start_index < 0 or input.post_count <= 0 or input.start_post_id < 0:  # 参数值非法
+                return output(112)
         except:
-            return output(111, False)
+            return output(111)
 
         try:
             db = getDb()
-            results = db.select('token', vars={'user_id': input.user_id, 'access_token': input.access_token},
+            results = db.select('token', vars={'user_id': input.user_id,
+                                               'access_token': input.access_token},
                                 where="user_id=$user_id and access_token=$access_token")
             # access_token权限不足
             if len(results) != 1:
-                return output(410, False)
+                return output(410)
 
-            # 查询是否存在start_post_id
-            post_ids = db.select('post', vars={'post_id': input.start_post_id},
-                                 where='post_id=$post_id', what='post_id')
-            if len(post_ids) != 1:
-                return output(467, False)  # start_post_id不存在
 
-            json_data = []
-            # 先定义json中data的post_list
+
             post_list = []
-            is_more = False
 
-            # start_post_id_index = input.start_post_id - input.start_index
-            # end_post_id_index = input.start_post_id - input.end_index-1
-            post_id_number = input.end_index - input.start_index + 2  # 为了判断是否又更多post而设计,如果还有post的话，则is_more=True
-            results = db.select('follow,post', vars={'user_id': input.user_id, 'post_id': input.start_post_id,
-                                                     "post_id_number": post_id_number},
-                                where="post.post_id>$post_id and follow.user_id=$user_id and follow.followed_id=post.user_id",
-                                what='post.user_id as post_user_id,add_time,content,post_id', order='post_user_id desc',
-                                limit="$post_id_number")
-            post_count_number = len(results)
-            if post_count_number == post_id_number:
-                is_more = True  # 判断时候有更多
-            for i in results:
-                post_user_id = i.post_user_id
-                post_user_info = db.select('userinfo',
-                                           vars={'user_id': post_user_id,
-                                                 'type1': 'nickname', 'type2': 'img_url'},
-                                           where='user_id=user_id and (type=$type1 or type=$type2)')
+            post_number = input.post_count + 1
+            what = "post.post_id as post_id, post.add_time as add_time, post.user_id as user_id, "
+            what += "post.content as content, post.img_url as img_url, "
+            what += "post.thumbnail_img_url as thumbnail_img_url"
 
-                # 下面实现可以预先存名字，以来加快系统响应
-                user_nickname = None
-                user_img_url = None
-                # 将user的昵称和url赋值，如果有为空的，则默认为上的的值。
-                # 显然这儿是两条记录，一条nickname，一条birthday，故用try包围。
-                # 目的是防止查表两次，这样的话查表一次就行。
-                for j in post_user_info:
-                    try:
-                        user_nickname = j.nickname
-                    except:
-                        pass
-                    try:
-                        user_img_url = j.img_url
-                    except:
-                        pass
-                user_mobile = '1234'
-                if user_nickname == None:  # 加上他的电话后四位。
-                    user_mobile = db.select('user', vars={'user_id': post_user_id}, where='user_id=$user_id',
-                                            what='mobile')
-                    # 用户电话必定有，否则系统错误。
-                    # 截取后四位
-                    try:
-                        user_mobile = str(user_mobile[0].mobile)[-4:]
-                    except:
-                        return output(700, False)
-                    user_nickname = '用户' + str(user_mobile)
+            if input.start_post_id == 0:
+                where = 'post.user_id=follow.followed_id and follow.user_id=$user_id'
+                results = db.select('post,follow', vars = {'user_id':input.user_id},
+                                    where = where,
+                                    what = what,
+                                    order = "post_id desc",
+                                    limit = "0,11")
+                for i in results:
+                    post_list.append({'user_id':i.user_id, 'post_id':i.post_id,
+                                      'add_time':str(i.add_time),
+                            'content':i.content, 'img_url':i.img_url,
+                            'thumbnail_img_url':i.thumbnail_img_url})
+
+                post_count = len(post_list)
+
+                if post_count == 11:
+                    post_count = 10
+                    post_list.pop()
+                    is_more = True
+                else:
+                    is_more = False
+
+                input.start_index = 0
+
+            else:
+                where = 'post.post_id<=$id and post.user_id=follow.followed_id '
+                where += 'and follow.user_id=$user_id'
+                results = db.select('post,follow', vars = {'id':input.start_post_id,
+                                                           'post_count':post_number,
+                                                           'start_index':input.start_index,
+                                                           'user_id':input.user_id},
+                                    where = where,
+                                    what = what,
+                                    order = "post_id desc",
+                                    limit = "$start_index, $post_count")
+
+                for i in results:
+                    post_list.append({'user_id':i.user_id, 'post_id':i.post_id,
+                                      'add_time':str(i.add_time),
+                            'content':i.content, 'img_url':i.img_url,
+                            'thumbnail_img_url':i.thumbnail_img_url})
+
+                post_count = len(post_list)
+
+                if post_count == post_number:
+                    post_count = post_number - 1
+                    post_list.pop()
+                    is_more = True
+                else:
+                    is_more = False
 
 
 
-                # 对该post_user_id的点赞总数目
-                favor_count_num = db.select('favor', vars={'post_id': post_user_id}, where='post_id=$post_id',
-                                            what='count(user_id)')
-                favor_count_num = int(len(favor_count_num))
+            for i in post_list:
+                results = db.select('userinfo', vars = {'id':i['user_id']},
+                                    where = "user_id=$id and type='nickname'",
+                                    what = "information")
+                if len(results) > 0:
+                    i['user_name'] = results[0].information
+                else:
+                    mobile = db.select('user', vars = {'id':i['user_id']},
+                                       where = "user_id=$id",
+                                       what = "mobile")[0].mobile
 
-                # 该post的评论总数目
-                comment_count_num = db.select('comments', vars={'post_id': post_user_id}, where='post_id=$post_id',
-                                              what='count(user_id)')
-                comment_count_num = int(len(comment_count_num))
+                    i['user_name'] = u"用户" + mobile[-4:]
 
-                # 文章图片的url列表
-                img_urls_list = []
-                img_urls_list_db = db.select('post_img', vars={'post_id': post_user_id}, where='post_id=$post_id',
-                                             what='img_url')
-                if len(img_urls_list_db) > 0:
-                    for img_i in img_urls_list_db:
-                        img_urls_list.append(img_i.img_url)
+                results = db.select('userinfo', vars = {'id':i['user_id']},
+                                where = "user_id=$id and type='img_url'",
+                                what = "information")
+                if len(results) > 0:
+                    i['user_img_url'] = results[0].information
+                else:
+                    i['user_img_url'] = None
 
-                post_list.append({"user_id": i.post_user_id, "user_img_url": user_img_url, "user_name": user_nickname,
-                                  "post_id": i.post_id, "add_time": str(i.add_time), "content": i.content,
-                                  "img_urls_list": img_urls_list, "favor_count": favor_count_num,
-                                  "comment_count": comment_count_num})
-            json_data.append(
-                {"start_post_id": input.start_post_id, "start_index": input.start_index,
-                 "post_count": post_count_number, "is_more": is_more, "post_list": post_list})
-            return output(200, json_data)
+                i['favor_count'] = db.select('favor', vars = {'id':i['post_id']},
+                                             where = "post_id=$id",
+                                             what = "count(*) as favor_count")[0].favor_count
+
+                i['comment_count'] = db.select('comments', vars = {'id':i['post_id']},
+                                             where = "post_id=$id",
+                                             what = "count(*) as comment_count")[0].comment_count
+            if len(post_list) > 0:
+                start_post_id = post_list[0]['post_id']
+            else:
+                start_post_id = 0
+            return output(200, {'start_post_id':start_post_id, "start_index":0,
+                                'post_count':post_count, 'is_more':is_more,
+                                'post_list':post_list})
 
         except:
-            return output(700, False)
+            return output(700)
 
--- sites/user/post/follow_update.py
+++ sites/user/post/follow_update.py
@@ -21,37 +21,35 @@ from verify import *
 class UserPostFollowUpdate:  # 查询关注用户的文章更新情况
     def POST(self):  # 传入access_token,user_id,last_post_id
         input = web.input(access_token=None, user_id=None, last_post_id=None)
-
-        # 缺少必填参数
-        if input.access_token == None or input.user_id == None or input.post_id == None:
+        if (input.access_token == None or input.user_id == None or input.last_post_id == None):
             return output(110, False)
+
         try:
             input.user_id = int(input.user_id)
-            input.last_post_id = int(input.post_id)
+            input.last_post_id = int(input.last_post_id)
         except:
             return output(111, False)
-        try:
-            db = getDb()
-            results = db.select('token', vars={'user_id': input.user_id, 'access_token': input.access_token},
-                                where="user_id=$user_id and access_token=$access_token")
-            # access_token权限不足
-            if len(results) != 1:
-                return output(410, False)
-            # 查询是否存在post_id
-            post_ids = db.select('post', vars={'post_id': input.last_post_id},
-                                 where='post_id=$post_id', what='post_id')
-            if len(post_ids) != 1:
-                return output(467, False)  # post_id不存在
-            # 查找user_id的所关注人的post_id
 
-            results = db.select('follow', vars={'user_id': input.user_id},
-                                where="user_id=$user_id")
-            for i in results:
-                followed_post_list = db.select('post', vars={'user_id': i.followed_id},
-                                               where="user_id=$user_id", what='post_id')
-                for j in followed_post_list:
-                    if int(input.last_post_id) < int(j.post_id):
-                        return output(200, True)
+        db = getDb()
+        results = db.select('token', vars = {'user_id': input.user_id, 'token':input.access_token},
+                               where="user_id=$user_id and access_token=$token")
+
+        if len(results) == 0:
+            return output(410, False)
+
+
+        try:
+            where = 'post.post_id<=$id and post.user_id=follow.followed_id '
+            where += 'and follow.user_id=$user_id'
+            results = db.select('post,follow', vars = {'id':input.last_post_id,
+                                                       'user_id':input.user_id},
+                                    where = where,
+                                    what = "post.post_id as post_id",
+                                    limit = "0,1")
+            if len(results) > 0:
+                return output(200, True)
+            else:
+                return output(200, False)
         except:
             return output(700, False)
 
