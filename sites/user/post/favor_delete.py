26b3b26ca8f3ca47924a7ebcc86db0c1133077d5
Alex Valentine <farseer810@163.com>
2015-08-13 14:43:48 +0800

    fix favor/delete

--- sites/user/post/favor_delete.py
+++ sites/user/post/favor_delete.py
@@ -16,18 +16,26 @@ from database import *
 @route.route('/user/post/favor/delete')
 class UserPostFavorAdd:
     def POST(self):  # 传入 access_token,user_id,post_id
-        input = web.input(access_token='', post_id='', user_id='');
-        if (input.access_token == '' or input.post_id == '' or input.user_id == ''):
-            return output(110);
+        input = web.input(access_token=None, post_id=None, user_id=None);
+        if (input.access_token == None or input.post_id == None or input.user_id == None):
+            return output(110)
+
+        try:
+            input.post_id = int(input.post_id)
+            input.user_id = int(input.user_id)
+        except:
+            return output(111)
+
         db = getDb()
-        user_id = db.select('token', vars={'access_token': input.access_token}, where="access_token=$access_token",
-                            what='user_id')
-        if len(user_id) == 0:
-            return output(412)
-        if_have = db.select('post', vars={'user_id': input.user_id, 'post_id': input.post_id})
-        # todo 未点赞，用户想要删除点赞,则返回啥？
-        if len(if_have) == 0:
-            return output(233)
+        results = db.select('token', vars={'access_token': input.access_token, 'user_id':input.user_id},
+                            where="access_token=$access_token and user_id=$user_id")
+        if len(results) == 0:
+            return output(410)
+
+
+        if len(db.select('post', vars={'post_id': input.post_id}, where = "post_id=$post_id")) == 0:
+            return output(467)
+
         try:
             db.delete('favor', vars={'post_id': input.post_id, 'user_id': input.user_id},
                       where="post_id=$post_id and user_id=$user_id")
