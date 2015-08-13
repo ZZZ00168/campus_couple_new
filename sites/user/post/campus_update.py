4285634cef388a03a27fbb4804978701eaac3233
Alex Valentine <farseer810@163.com>
2015-08-13 15:20:00 +0800

    fix campus/update

--- sites/user/post/campus_update.py
+++ sites/user/post/campus_update.py
@@ -18,40 +18,40 @@ from database import *
 @route.route('/user/post/campus/update')
 class UserPostCampusUpdate:  # 查询校区文章更新情况
     def POST(self):
-        input = web.input(access_token=None, user_id=None, Last_post_id=None)
-        if (input.access_token == None or input.user_id == None or input.Last_post_id == None):
-            return output(110)
-        db = getDb()
-        try:
-            result = db.select('token', vars={'user_id': input.user_id}, where="user_id=$user_id", what="access_token")
-        except:
-            return output(700)
-        if len(result) != 1:
-            return output(700)
-        for i in result:
-            if i.access_token != input.access_token:
-                return output(410)
-        try:
-            # result=db.select('post,user',vars={'user_id':input.user_id},where="post.user_id=user.user_id and user.")
-            campus = db.select('user', vars={'user_id': input.user_id}, where="user_id=$user_id", what="campus_id")
-        except:
-            return output(700)
-        if len(campus) != 1:
-            return output(700)
-        for i in campus:
-            campus_id = i.campus_id
-        try:
-            post_list= db.select('user,post', vars={'campus_id': campus_id},
-                                  where="user.user_id=post.user_id and user.campus_id=$campus_id", what="post.post_id")
-        except:
-            return output(700)
-        if len(post_list)==0:
-            return output(467)
-        maxId=0
-        for i in post_list:
-            maxId=max(i.post_id,maxId)
-        if int(maxId)<=int(input.Last_post_id):
-             return output(200,False)
-        else:
-             return output(200,True)
+        input = web.input(access_token=None, user_id=None, last_post_id=None)
+        if (input.access_token == None or input.user_id == None or input.last_post_id == None):
+            return output(110, False)
 
+        try:
+            input.user_id = int(input.user_id)
+            input.last_post_id = int(input.last_post_id)
+        except:
+            return output(111, False)
+
+        db = getDb()
+        results = db.select('token', vars = {'user_id': input.user_id, 'token':input.access_token},
+                               where="user_id=$user_id access_token=$token")
+
+        if len(results) == 0:
+            return output(410, False)
+
+        campus_id = db.select('user', vars = {'id' : input.user_id}, where = "user_id=$id",
+                                what = 'campus_id')[0].campus_id
+        try:
+            campus_id = int(campus_id)
+            if campus_id <= 0:
+                return output(460, False)
+        except:
+            return output(460, False)
+
+
+        results = db.select('post,user', vars = {'id':input.last_post_id,
+                                                 'campus_id':campus_id},
+                                where = "post_id>$id and user.user_id=post.user_id and user.campus_id=$campus_id",
+                                what = "post.post_id as post_id",
+                                limit = "0, 1")
+
+        if len(results) > 0:
+            return output(200, True)
+        else:
+            return output(200, False)
