17eb384a894b53736fb5d736c252330a40049b34
Alex Valentine <farseer810@163.com>
2015-08-15 17:07:53 +0800

    add GET /campus/food/add

--- sites/campus/food/add_delete.py
+++ sites/campus/food/add_delete.py
@@ -41,6 +41,24 @@ class FoodAdd:
             return output(700)
         return output(200, food_id)
 
+    def GET(self):
+        html_text = """<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
+    <form action="/campus/food/add" method="post" enctype="multipart/form-data">
+        access_token: <input name="access_token" type="text"/><br/>
+        food_name: &nbsp;&nbsp;&nbsp;<input name="food_name" type="text"/><br/>
+        food_price: &nbsp;&nbsp;<input name="food_price" type="text"/><br/>
+        food_desc: &nbsp;&nbsp;&nbsp;<input name="food_desc" type="text"/><br/>
+        <input type="submit" value="submit"/>
+    </form>
+</body>
+</html>
+"""
+        return html_text
+
 
 @route.route('/campus/food/delete')
 class FoodDelete:
